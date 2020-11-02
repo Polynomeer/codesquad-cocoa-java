9033d8ad5fa0df3eacf66b70f7a7f94d29252755
polynomeer <polynomeer@gmail.com>
2020-11-02 17:32:49 +0900

    1st solved BOJ.11720

---
+++ mission1/Loop/src/bjs/cocoa/codesquad/BOJ_11720.java
@@ -0,0 +1,25 @@
+/**
+ * BOJ 11720번 문제풀이
+ *
+ * @author Polynomeer
+ * @version 1.0, 1st solved BOJ.11720
+ * @caution 클래스 이름을 Main으로 변경 후 채점 가능
+ */
+package bjs.cocoa.codesquad;
+
+import java.util.Scanner;
+
+public class BOJ_11720 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String num = sc.next();
+
+        int sum = 0;
+        for ( int i = 0; i < N; i++ ) {
+            sum += num.charAt(i) - '0';
+        }
+        System.out.println(sum);
+    }
+
+}
