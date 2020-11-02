f54a0490a19090c5002c0e27c00fcd4abeb5ce33
polynomeer <polynomeer@gmail.com>
2020-11-02 16:29:07 +0900

    1st solved BOJ.14681

---
+++ mission1/Conditional/src/bjs/cocoa/codesquad/BOJ_14681.java
@@ -0,0 +1,30 @@
+/**
+ * BOJ 14681번 문제풀이
+ *
+ * @author Polynomeer
+ * @version 1.0, 1st solved BOJ.14681
+ * @caution 클래스 이름을 Main으로 변경 후 채점 가능
+ */
+package bjs.cocoa.codesquad;
+
+import java.util.Scanner;
+
+public class BOJ_14681 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        int quadrant = getQuadrant(x, y);
+        System.out.println(quadrant);
+    }
+
+    static int getQuadrant(int x, int y) {
+        if (x > 0 && y > 0) return 1;
+        if (x < 0 && y > 0) return 2;
+        if (x < 0 && y < 0) return 3;
+        if (x > 0 && y < 0) return 4;
+        return 0;
+    }
+
+}
