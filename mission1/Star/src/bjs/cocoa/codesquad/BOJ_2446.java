dd656d710358e93f6ee1ddd58511b9937785e156
polynomeer <polynomeer@gmail.com>
2020-11-03 16:18:54 +0900

    1st solved BOJ.2446

---
+++ mission1/Star/src/bjs/cocoa/codesquad/BOJ_2446.java
@@ -0,0 +1,44 @@
+/**
+ * BOJ 2446번 문제풀이
+ *
+ * @author Polynomeer
+ * @version 1.0, 1st solved BOJ.2446
+ * @caution 클래스 이름을 Main으로 변경 후 채점 가능
+ */
+package bjs.cocoa.codesquad;
+
+import java.util.Scanner;
+
+public class BOJ_2446 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j < N; j++) {
+                System.out.print("*");
+            }
+            for (int j = i; j <= N; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = i + 1; j < N - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i + 1; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
