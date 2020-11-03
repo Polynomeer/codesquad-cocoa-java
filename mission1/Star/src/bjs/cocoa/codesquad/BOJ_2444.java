3e5f9c50c3ea10c4771f100a286e58a87a89895c
polynomeer <polynomeer@gmail.com>
2020-11-03 15:53:59 +0900

    1st solved BOJ.2444

---
+++ mission1/Star/src/bjs/cocoa/codesquad/BOJ_2444.java
@@ -0,0 +1,43 @@
+/**
+ * BOJ 2444번 문제풀이
+ *
+ * @author Polynomeer
+ * @version 1.0, 1st solved BOJ.2444
+ * @caution 클래스 이름을 Main으로 변경 후 채점 가능
+ */
+package bjs.cocoa.codesquad;
+
+import java.util.Scanner;
+
+public class BOJ_2444 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j < N; j++) {
+                System.out.print("*");
+            }
+            for (int j = i; j < N - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
