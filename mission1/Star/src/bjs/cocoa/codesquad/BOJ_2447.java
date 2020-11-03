e34b8ec198b50cd914a0ea4a6f738c72e9f72d41
polynomeer <polynomeer@gmail.com>
2020-11-03 16:34:42 +0900

    1st solved BOJ.2522

---
+++ mission1/Star/src/bjs/cocoa/codesquad/BOJ_2447.java
@@ -0,0 +1,19 @@
+/**
+ * BOJ 2447번 문제풀이
+ *
+ * @author Polynomeer
+ * @version 1.0, 1st solved BOJ.2447
+ * @caution 클래스 이름을 Main으로 변경 후 채점 가능
+ */
+package bjs.cocoa.codesquad;
+
+import java.util.Scanner;
+
+public class BOJ_2447 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        sc.close();
+    }
+}
---
+++ mission1/Star/src/bjs/cocoa/codesquad/BOJ_2448.java
@@ -0,0 +1,19 @@
+/**
+ * BOJ 2448번 문제풀이
+ *
+ * @author Polynomeer
+ * @version 1.0, 1st solved BOJ.2448
+ * @caution 클래스 이름을 Main으로 변경 후 채점 가능
+ */
+package bjs.cocoa.codesquad;
+
+import java.util.Scanner;
+
+public class BOJ_2448 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        sc.close();
+    }
+}
