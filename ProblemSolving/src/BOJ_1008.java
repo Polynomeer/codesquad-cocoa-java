5a2d264f22369246ee2cb4bd986406cd80fde954
polynomeer <polynomeer@gmail.com>
2020-11-06 11:09:23 +0900

    1st Solved BOJ.2438

--- ProblemSolving/src/BOJ_1008.java
+++ ProblemSolving/src/BOJ_1008.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class BOJ_1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double A = sc.nextInt();
+        double B = sc.nextInt();
+
+        System.out.println(A / B);
+        sc.close();
+    }
 }
