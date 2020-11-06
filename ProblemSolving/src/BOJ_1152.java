9a404c4c507de487a4f5ad29b2e90331cc8dd5f1
polynomeer <polynomeer@gmail.com>
2020-11-06 14:20:44 +0900

    add README.md int ProblemSolving

---
+++ ProblemSolving/src/BOJ_1152.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class BOJ_1152 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        Map<String, Integer> words = new HashMap<String, Integer>();
+
+    }
+}
---
+++ ProblemSolving/src/README.md
@@ -0,0 +1,7 @@
+# Study Note
+
+## Simple note with problem solving
+
+### BOJ.11654
+Scanner, InputStream, BufferedReader
+- https://st-lab.tistory.com/41?category=830901
