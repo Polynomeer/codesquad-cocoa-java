b6ca9e46c63f57147ffe6d683da0b48582216f02
polynomeer <polynomeer@gmail.com>
2020-11-06 15:14:18 +0900

    1st failed BOJ.1152 with space problem

--- ProblemSolving/src/BOJ_1152.java
+++ ProblemSolving/src/BOJ_1152.java
@@ -1,13 +1,17 @@
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class BOJ_1152 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
+        sc.close();
 
-        Map<String, Integer> words = new HashMap<String, Integer>();
+        // split string by space
+        String[] strArr = str.split(" ");
 
+        for (String s : strArr) {
+            System.out.println(s);
+        }
+        System.out.println(strArr.length);
     }
 }
