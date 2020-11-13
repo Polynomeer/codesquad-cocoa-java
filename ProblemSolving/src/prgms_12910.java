8da400a5d98960f65453d6e8afa2b1357e9b6599
polynomeer <polynomeer@gmail.com>
2020-11-13 14:42:00 +0900

    1st solved programmers.12910

---
+++ ProblemSolving/src/prgms_12910.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class prgms_12910 {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.solution(new int[]{5, 9, 7, 10}, 5));
+        System.out.println(sol.solution(new int[]{2, 36, 1, 3}, 1));
+        System.out.println(sol.solution(new int[]{3, 2, 6}, 10));
+    }
+}
+
+class Solution {
+    public ArrayList<Integer> solution(int[] arr, int divisor) {
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        for (int i : arr) {
+            if (i % divisor == 0) {
+                answer.add(i);
+            }
+        }
+        Collections.sort(answer);
+        if (answer.size() == 0) {
+            answer.add(-1);
+        }
+
+        return answer;
+    }
+}
