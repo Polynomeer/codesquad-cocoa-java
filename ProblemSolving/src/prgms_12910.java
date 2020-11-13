c55a2c525c73a04143edec34f63b08d192d9e41e
polynomeer <polynomeer@gmail.com>
2020-11-13 15:25:26 +0900

    1st solved programmers.64061

--- ProblemSolving/src/prgms_12910.java
+++ ProblemSolving/src/prgms_12910.java
@@ -3,14 +3,14 @@ import java.util.Collections;
 
 public class prgms_12910 {
     public static void main(String[] args) {
-        Solution sol = new Solution();
+        Solution_12910 sol = new Solution_12910();
         System.out.println(sol.solution(new int[]{5, 9, 7, 10}, 5));
         System.out.println(sol.solution(new int[]{2, 36, 1, 3}, 1));
         System.out.println(sol.solution(new int[]{3, 2, 6}, 10));
     }
 }
 
-class Solution {
+class Solution_12910 {
     public ArrayList<Integer> solution(int[] arr, int divisor) {
         ArrayList<Integer> answer = new ArrayList<>();
 
