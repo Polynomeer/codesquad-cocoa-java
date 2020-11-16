244a28713672d9dc374b8c82b79f14d85b857ebb
polynomeer <polynomeer@gmail.com>
2020-11-16 17:07:23 +0900

    1st solved programmers.42748

--- ProblemSolving/src/prgms/prgms_42748.java
+++ ProblemSolving/src/prgms/prgms_42748.java
@@ -10,12 +10,10 @@ public class prgms_42748 {
     public static void main(String[] args) {
         Solution_42748 sol = new Solution_42748();
         System.out.println(
-
                 sol.solution(
                         new int[]{1, 5, 2, 6, 3, 7, 4},
                         new int[][]{{2, 5, 3}, {4, 4, 1}, {1, 7, 3}}
                 )
-
         );
 
     }
