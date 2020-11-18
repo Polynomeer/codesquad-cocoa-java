173fdf59b15b8f7b065e528ac9aa4a9bb8239498
polynomeer <polynomeer@gmail.com>
2020-11-18 14:44:25 +0900

    Added MyCalendar Class

--- mission5/src/clock/ClockExample.java
+++ mission5/src/clock/ClockExample.java
@@ -1,15 +1,48 @@
 package clock;
 
-public class ClockExample {
-    public static void main(String[] args) {
-        HangulClock hangulClock = new HangulClock();
+import java.util.Scanner;
 
+public class ClockExample {
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        while (true){
+            System.out.println("What do you want to print? (clock: Hangul clock, cal: Calendar, q: quit)");
+            System.out.print(">> ");
+            String command = sc.nextLine();
+            String[] commands = command.split(" ");
+
+            if (command.equals("q")) break;
+            if (commands[0].equals("clock")) {
+                runClock();
+            } else if (commands[0].equals("cal")) {
+                runCalendar(commands);
+            }
+        }
+        sc.close();
+    }
+
+    private static void runCalendar(String[] args) {
+        MyCalendar myCalendar = new MyCalendar();
+        if (args.length == 1) {
+            myCalendar.printCalendar();
+            return;
+        }
+        if (args.length >= 3){
+            int year = Integer.parseInt(args[1]);
+            int month = Integer.parseInt(args[2]);
+            myCalendar.printCalendar(year, month);
+            return;
+        }
+    }
+
+    public static void runClock() {
+        HangulClock hangulClock = new HangulClock();
         while (true) {
             try {
                 hangulClock.clearScreen();
                 hangulClock.printScreen(hangulClock.getCalendar());
                 Thread.sleep(60000);
-
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
--- mission5/src/clock/MyCalendar.java
+++ mission5/src/clock/MyCalendar.java
@@ -1,4 +1,73 @@
 package clock;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class MyCalendar {
+    private final String ANSI_RESET = "\u001B[0m";
+    private final String ANSI_GREEN = "\u001B[32m";
+    private Calendar calendar;
+    private int dayOfWeek;
+    private int daysInMonth;
+
+    public MyCalendar() {
+        this.calendar = new GregorianCalendar();
+        Date date = new Date();
+        date.setTime(System.currentTimeMillis());
+        calendar.setTime(date);
+
+        // get day of week for 1st of the month
+        this.dayOfWeek = calendar.get(calendar.DAY_OF_WEEK);
+        this.daysInMonth = calendar.getActualMaximum(calendar.DAY_OF_MONTH);
+    }
+
+    public void printCalendar() {
+        printColumn();
+        printSpace();
+        printDays();
+    }
+
+    public void printCalendar(int year, int month) {
+        this.calendar.set(year, month, 0);
+        this.dayOfWeek = calendar.get(calendar.DAY_OF_WEEK);
+        this.daysInMonth = calendar.getActualMaximum(calendar.DAY_OF_MONTH);
+
+        printColumn();
+        printSpace();
+        printDays();
+    }
+
+    private void printColumn() {
+        // print columns (month, year, week)
+        System.out.println(new SimpleDateFormat("MMMM YYYY").format(calendar.getTime()));
+        System.out.println(" Sun  Mon  Tue  Wed  Thu  Fri  Sat");
+    }
+
+    private void printSpace() {
+        // print initial spaces
+        String initSpace = "";
+        for (int i = 0; i < dayOfWeek - 1; i++) {
+            initSpace += "     ";
+        }
+        System.out.print(initSpace);
+    }
+
+    private void printDays() {
+        // print the days of the month starting from 1
+        for (int dayOfMonth = 1; dayOfMonth <= daysInMonth; ) {
+            for (int j = ((dayOfMonth == 1) ? dayOfWeek - 1 : 0); j < 7 && (dayOfMonth <= daysInMonth); j++) {
+                if (calendar.get(Calendar.DATE) == dayOfMonth) {
+                    String space = calendar.get(Calendar.DATE) >= 10 ? "  " : "   ";
+                    System.out.print(space + ANSI_GREEN + dayOfMonth + ANSI_RESET + " ");
+                } else {
+                    System.out.printf("%4d ", dayOfMonth);
+                }
+                dayOfMonth++;
+            }
+            System.out.println();
+        }
+    }
+
 }
