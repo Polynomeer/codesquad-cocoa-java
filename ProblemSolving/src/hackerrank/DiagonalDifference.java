5b2942f2384c8bb8808544a7589771ca2e739e53
polynomeer <polynomeer@gmail.com>
2020-11-20 15:12:49 +0900

    1st solved Hacker Rank - Diagonal Difference

--- ProblemSolving/src/hackerrank/DiagonalDifference.java
+++ ProblemSolving/src/hackerrank/DiagonalDifference.java
@@ -15,17 +15,17 @@ import static java.util.stream.Collectors.toList;
 
 class DiagonalDifferenceResult {
 
-    /*
-     * Complete the 'diagonalDifference' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
-     */
-
     public static int diagonalDifference(List<List<Integer>> arr) {
-        // Write your code here
         int answer = 0;
-        return answer;
+        int primaryDiagonal = 0;
+        int secondaryDiagonal = 0;
+
+        for (int i = 0, j = 0; i < arr.size(); i++, j++) {
+            primaryDiagonal += arr.get(i).get(j);
+            secondaryDiagonal += arr.get(i).get(arr.size() - j - 1);
+        }
+        answer = primaryDiagonal - secondaryDiagonal;
+        return answer > 0 ? answer : -answer;
     }
 
 }
