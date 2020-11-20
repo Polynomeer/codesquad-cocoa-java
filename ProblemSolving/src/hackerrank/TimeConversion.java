9e3aeec05dfe17355a35ad8e06b1f234579968c7
polynomeer <polynomeer@gmail.com>
2020-11-20 13:04:46 +0900

    1st solved Hacker Rank - Time Conversion

--- ProblemSolving/src/hackerrank/TimeConversion.java
+++ ProblemSolving/src/hackerrank/TimeConversion.java
@@ -8,14 +8,30 @@ import java.util.regex.*;
 
 public class TimeConversion {
 
-    /*
-     * Complete the timeConversion function below.
-     */
     static String timeConversion(String s) {
-        /*
-         * Write your code here.
-         */
-        String answer = "";
+        String answer = s.substring(0, 8);
+        String noon = s.substring(8);
+        int hour = Integer.parseInt(s.substring(0, 2));
+
+        if (hour == 12 && noon.equals("PM")) {
+            return answer;
+        }
+        if (hour == 12 && noon.equals("AM")) {
+            hour -= 12;
+        }
+        if (noon.equals("PM")) {
+            hour += 12;
+        }
+
+        StringBuffer sb = new StringBuffer(answer);
+
+        if (hour < 10) {
+            sb.replace(0, 2, "0" + Integer.toString(hour));
+        } else {
+            sb.replace(0, 2, Integer.toString(hour));
+        }
+        answer = sb.toString();
+
         return answer;
     }
 
@@ -27,6 +43,8 @@ public class TimeConversion {
         String s = scan.nextLine();
 
         String result = timeConversion(s);
+
+        System.out.println(result);
 
         bw.write(result);
         bw.newLine();
