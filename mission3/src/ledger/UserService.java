5193695b2e88ccdfe35aa5d5a99d1a543ed2b33a
polynomeer <polynomeer@gmail.com>
2020-11-26 01:47:12 +0900

    feat: Add initUserList() to UserService class

--- mission3/src/ledger/UserService.java
+++ mission3/src/ledger/UserService.java
@@ -1,37 +1,67 @@
 package ledger;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class UserService {
 
-    private UserDAO userDAO;
     private Scanner sc;
     private List<UserVO> userList;
 
-    public UserService() {
-        userDAO = new UserDAO();
-        sc = new Scanner(System.in);
+    public UserService() throws FileNotFoundException {
         userList = new ArrayList<>();
+        initUserList();
+        sc = new Scanner(System.in);
     }
 
-    private void process(){
+    private void initUserList() throws FileNotFoundException {
+        File file = new File("./user.csv"); // create File instance
+        sc = new Scanner(file); // read file by Scanner
 
-        while (!Main.isExit) {
-                System.out.println("⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤");
-                System.out.println("                    Your Financial ledger.Ledger");
-                System.out.println("Please register your account.");
-                System.out.println("⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤");
+        while (sc.hasNextLine()) {
+            String[] line = sc.nextLine().split(",");
+            String username = line[0];
+            String password = line[1];
 
-                System.out.print("user name : ");
-                String username = sc.next();
-                System.out.print("user password : ");
-                String password = sc.next();
-                UserVO userVO = new UserVO(username, password);
-
-                continue;
+            UserVO userVO = new UserVO(username,password);
+            System.out.println(userVO);
+            userList.add(userVO);
         }
+        sc.close();
+
+    }
+
+    private void process() {
+
+        System.out.println("⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤");
+        System.out.println("                    Your Financial Ledger");
+        System.out.println("1. sign up / 2. sign in / 3. exit");
+        System.out.println("⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤");
+        int option = sc.nextInt();
+
+        if (option == 1){
+            System.out.println("⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤");
+            System.out.println("                    Your Financial ledger.Ledger");
+            System.out.println("Please register your account.");
+            System.out.println("⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤");
+
+            System.out.print("user name : ");
+            String username = sc.next();
+            System.out.print("user password : ");
+            String password = sc.next();
+            UserVO userVO = new UserVO(username, password);
+        }
+        if (option == 2){
+
+        }
+        if (option == 3){
+            Main.isExit = true;
+        }
+
+
         sc.close();
     }
 
