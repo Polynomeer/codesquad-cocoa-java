c61c1d505cc4e1a961498afd4898e100208d9ecc
polynomeer <polynomeer@gmail.com>
2020-11-27 12:10:09 +0900

    Solve LeetCode - Reverse Integer

--- ProblemSolving/src/leetcode/ReverseInteger.java
+++ ProblemSolving/src/leetcode/ReverseInteger.java
@@ -2,21 +2,24 @@ package leetcode;
 
 public class ReverseInteger {
     public static int reverse(int x) {
-        int answer = 0;
+        long answer = 0;
+        final int MAX_INT = 2147483647;
 
         while (x != 0) {
             answer *= 10;
             answer += x % 10;
+            if (answer > MAX_INT || answer < -MAX_INT) return 0; // returns 0 when the reversed integer overflows
             x /= 10;
         }
-
-        return answer;
+        return (int) answer;
     }
 
     public static void main(String[] args) {
         System.out.println(reverse(123));
         System.out.println(reverse(-123));
         System.out.println(reverse(120));
+        System.out.println(reverse(1534236469));
+        System.out.println(reverse(1463847412));
     }
 
 }
